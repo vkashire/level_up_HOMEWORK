091273db618ed0cb1948c84f8269815d127ef8fe
Vesta Kashirest <vesta13lein@google.com>
2020-10-19 17:59:32 +0300

    lesson 2 homework: программа, выводящая все четырёхзначные числа последовательности 1000 1003 1006 1009 1012 1015

--- src/lecture2/FourDigitNumberInSequense.java
+++ src/lecture2/FourDigitNumberInSequense.java
@@ -1,4 +1,25 @@
 package lecture2;
 
+import java.util.Arrays;
+
 public class FourDigitNumberInSequense {
+
+    public static void main(String[] args){
+
+        //create an array
+        int[] list = new int[5000];
+        list[0] = 1000;
+
+        //fill the array, use for loop
+        for(int i = 1; i < list.length; i++){
+            list[i] = list[i-1] + 3;
+        }
+        System.out.println(Arrays.toString(list));
+
+         //show all four-digit numbers
+        for (int i = 0; list[i] >= 1000 && list[i] <= 9999; i++) {
+            System.out.println(list[i]);
+        }
+
+    }
 }
