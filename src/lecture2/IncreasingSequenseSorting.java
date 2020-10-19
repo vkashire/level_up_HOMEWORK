1d894aa740a1eee9becf41445e66af1e0445e29d
Vesta Kashirest <vesta13lein@google.com>
2020-10-19 18:21:17 +0300

    lesson 2 homework: сортировка по возрастанию переменных a, b и c

--- src/lecture2/IncreasingSequenseSorting.java
+++ src/lecture2/IncreasingSequenseSorting.java
@@ -1,4 +1,23 @@
 package lecture2;
 
+import java.util.Arrays;
+
 public class IncreasingSequenseSorting {
+    public static void main(String[] args){
+
+     //write values foe a,b,c
+     int a = -2;
+     int b = 3;
+     int c = -5;
+
+     //convert a,b,c valuec into array
+     int[] list = new int[]{a,b,c};
+
+     //sort arrays elements
+     Arrays.sort(list);
+
+     //print array
+     System.out.println(Arrays.toString(list));
+
+    }
 }
