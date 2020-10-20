ea63e3971ea21f2083cbcc5de24860fef5ad9f11
Vesta Kashirest <vesta13lein@google.com>
2020-10-20 15:31:40 +0300

    lesson 2 homework: программа проверяет попало ли случайно выбранное из отрезка [5;155] целое число в интервал (25;100) и сообщать результат на экран.

--- src/lecture2/NumberInNumberSection.java
+++ src/lecture2/NumberInNumberSection.java
@@ -1,4 +1,14 @@
 package lecture2;
 
 public class NumberInNumberSection {
+
+    public static void main(String[] args){
+        int x = (int)(5 + Math.random()*151);
+
+        if(x > 25 && x < 100){
+            System.out.println("число " + x + " содержится в интервале (25, 100)");
+        } else {
+            System.out.println("число " + x + " не содержится в интервале (25, 100)");
+        }
+    }
 }
