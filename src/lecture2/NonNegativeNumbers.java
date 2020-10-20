afc990a1400784b7bedfd8b2c46d33c9fdbfc91b
Vesta Kashirest <vesta13lein@google.com>
2020-10-20 15:18:35 +0300

    lesson 2 homework: программа выводит на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60...

--- src/lecture2/NonNegativeNumbers.java
+++ src/lecture2/NonNegativeNumbers.java
@@ -1,4 +1,24 @@
 package lecture2;
 
+import java.util.Arrays;
+
 public class NonNegativeNumbers {
+
+    public static void main(String[] args) {
+        //create new array
+        int[] sequence = new int[25];
+
+        //fill the array with necessary values
+        sequence[0] = 90;
+
+        for(int i = 1; i < sequence.length; i++){
+            sequence[i] = sequence[i-1] - 5;
+        }
+
+        //print positive numbers
+        for(int i = 0; sequence[i] > 0; i++) {
+            System.out.println(sequence[i]);
+        }
+
+    }
 }
