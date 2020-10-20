977ea3ce60ab94617d3ad1e3bfa93bc589c093c7
Vesta Kashirest <vesta13lein@google.com>
2020-10-20 17:34:53 +0300

    lesson 2, homework №7: программа выводит на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 …

--- src/lecture2/OddSequenseElements.java
+++ src/lecture2/OddSequenseElements.java
@@ -1,4 +1,25 @@
 package lecture2;
 
+import java.util.Arrays;
+
 public class OddSequenseElements {
+
+    public static void main(String[] args){
+
+        //create an array
+        int[] list = new int[60];
+
+        //fill the array
+        list[0] = 1;
+        for(int i = 1; i < list.length; i++ ){
+            list[i] = list[i - 1] + 2;
+            //System.out.println(list[i]);
+        }
+
+        //print first 55 elements
+        for(int i = 0; i < 55; i++){
+            System.out.println(list[i]);
+        }
+
+    }
 }
