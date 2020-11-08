ba90f97d323ed83b636833031e63e61b70da2b9d
Vesta Kashirest <vesta13lein@google.com>
2020-11-08 22:29:37 +0300

    Homework 3, Array sorting corrected.

--- src/lecture3/ArraySorting.java
+++ src/lecture3/ArraySorting.java
@@ -1,35 +1,23 @@
 package lecture3;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArraySorting {
 
-public int[] bubbleSort(int[] array){
-int [] massiv = new int[]{array.length};
-    for(int i = 1; i < array.length; i++){
-        if (array[i] > array[ i - 1 ]){
-            int x = array[ i ];
-            int y = array[ i - 1 ];
-            massiv[i] = y;
-            massiv[ i - 1 ] = x;
-            break;
+    public void bubbleSort(int[] array) {
+        int[] massiv = new int[array.length + 1];
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int x = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = x;
+                }
+            }
+            }
+        System.out.println("новый массив: " + Arrays.toString(array));
         }
+
     }
 
-System.out.println("новый массив: " + Arrays.toString(massiv));
-return massiv;
-}
 
-//private void toSwap(int firstNumber, int secondNumber){
-//    int temp = firstNumber;
-//    firstNumber = secondNumber;
-//    secondNumber = temp;
-//}
-
-}
-
-
-//for (int j = i + 1; j < glassesOfWine.length; j++, i++){
-//        glassesOfWine[i] = glassesOfWine[j];
-//        }
